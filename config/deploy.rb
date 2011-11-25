e7b315a63526ab46efbc3ada5ecab9b586dd54e3
xdite <xuite.joke@gmail.com>
2011-11-26 01:20:46 +0800

    add remote rake

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,6 +44,12 @@ end
 task :restart_resque, :roles => :web do
   run "cd #{deploy_to}/current/; RAILS_ENV=production ./script/resque stop; RAILS_ENV=production ./script/resque start"
 end
+namespace :remote_rake do
+  desc "Run a task on remote servers, ex: cap staging rake:invoke task=cache:clear"
+  task :invoke do
+    run "cd #{deploy_to}/current; RAILS_ENV=#{rails_env} bundle exec rake #{ENV['task']}"
+  end
+end
 
 task :install_gems, :roles => :web do  	
   run "cd #{deploy_to}/current/; bundle install"	  	
