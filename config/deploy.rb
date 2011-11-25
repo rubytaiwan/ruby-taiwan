045efdf5c1b54455d8b6e1341bcd9e74374e9ae0
xdite <xuite.joke@gmail.com>
2011-11-26 00:40:26 +0800

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,33 +1,28 @@
 # coding: utf-8
-set :application, "ruby-china"
-set :repository,  "git://github.com/huacnlee/ruby-china.git"
-set :branch, "master"
+require "bundler/capistrano"
+default_environment["RAILS_ENV"] = "production"
+default_environment["PATH"] = "/usr/local/bin:/usr/bin:/bin"
+
+set :application, "ruby-taiwan"
+set :repository,  "git://github.com/xdite/ruby-taiwan.git"
+set :branch, "production"
 set :scm, :git
-set :user, "ruby"
-set :deploy_to, "/home/#{user}/www/#{application}"
-set :runner, "ruby"
+set :user, "apps"
+set :deploy_to, "/home/apps/#{application}"
+set :runner, "apps"
 set :deploy_via, :remote_cache
 set :git_shallow_clone, 1
+set :use_sudo, false
 
-role :web, "58.215.172.218"                          # Your HTTP server, Apache/etc
-role :app, "58.215.172.218"                          # This may be the same as your `Web` server
-role :db,  "58.215.172.218", :primary => true # This is where Rails migrations will run
-
-# unicorn.rb 路径
-set :unicorn_path, "#{deploy_to}/current/config/unicorn.rb"
+role :web, "ruby-taiwan.org"                          # Your HTTP server, Apache/etc
+role :app, "ruby-taiwan.org"                          # This may be the same as your `Web` server
+role :db,  "ruby-taiwan.org", :primary => true # This is where Rails migrations will run
 
 namespace :deploy do
-  task :start, :roles => :app do
-    run "cd #{deploy_to}/current/; RAILS_ENV=production unicorn_rails -c #{unicorn_path} -D"
-  end
 
-  task :stop, :roles => :app do
-    run "kill -QUIT `cat #{deploy_to}/current/tmp/pids/unicorn.pid`"
-  end
-
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "kill -USR2 `cat #{deploy_to}/current/tmp/pids/unicorn.pid`"
+  desc "Restart passenger process"
+  task :restart, :roles => [:web], :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
   end
 end
 
@@ -39,11 +34,6 @@ end
 
 task :link_shared_config_yaml, :roles => :web do
   run "ln -sf #{deploy_to}/shared/config/*.yml #{deploy_to}/current/config/"
-  run "ln -sf #{deploy_to}/shared/config/unicorn.rb #{deploy_to}/current/config/"
-end
-
-task :install_gems, :roles => :web do
-  run "cd #{deploy_to}/current/; bundle install"
 end
 
 task :restart_resque, :roles => :web do
@@ -61,10 +51,3 @@ end
 
 after "deploy:symlink", :init_shared_path, :link_shared_config_yaml, :install_gems, :compile_assets
 
-
-set :default_environment, { 
-  'PATH' => "/home/ruby/.rvm/gems/ruby-1.9.3-p0/bin:/home/ruby/.rvm/gems/ruby-1.9.3-p0@global/bin:/home/ruby/.rvm/rubies/ruby-1.9.3-p0/bin:/home/ruby/.rvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games",
-  'RUBY_VERSION' => 'ruby-1.9.3-p0',
-  'GEM_HOME' => '/home/ruby/.rvm/gems/ruby-1.9.3-p0',
-  'GEM_PATH' => '/home/ruby/.rvm/gems/ruby-1.9.3-p0:/home/ruby/.rvm/gems/ruby-1.9.3-p0@global'
-}
