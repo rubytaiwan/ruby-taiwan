c58bf2cfe9af8086a8a9abdbe268da0c046f8c75
chitsaou <chitsaou@techbang.com.tw>
2011-12-01 13:58:13 +0800

    把寄送主題回覆通知的程式移到 Reply model 的 after_create hook

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -22,7 +22,6 @@ class RepliesController < ApplicationController
       #       the latest reply and its author,
       #       and adds the reply author to the followers list.
 
-      send_notify_reply_mail(@topic, @reply, :exclude_mentioned => true)
     else
       @msg = @reply.errors.full_messages.join("<br />")
     end
@@ -50,30 +49,4 @@ class RepliesController < ApplicationController
     @topic = Topic.find(params[:topic_id])
   end
 
-  def send_notify_reply_mail(topic, reply, options={})
-
-    # set :exclude_mentioned to true to exclude mentioned users
-    # from the reply notification mail recipients
-    options[:exclude_mentioned] ||= false
-
-    # fetch follower ids from the topic (may or may not include the topic author)
-    recipient_ids = Set.new(topic.follower_ids)
-
-    # don't send reply notification to the author of the reply
-    recipient_ids.delete(reply.user.id)
-
-    # prevent duplicated mail sent to users mentioned in the reply
-    recipient_ids.subtract(reply.mentioned_user_ids) if options[:exclude_mentioned] == true
-
-    # add the topic author to the recipients, if he is not the reply author
-    recipient_ids.add(topic.user.id) if topic.user.id != reply.user.id
-
-    # find recipient users
-    recipients = User.find(recipient_ids.to_a)
-
-    recipients.each do |recipient|
-      next if recipient == nil
-      TopicMailer.notify_reply(recipient, topic, reply).deliver
-    end
-  end
 end
