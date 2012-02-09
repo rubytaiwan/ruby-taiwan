ad7e3aa0c2a701c0d0a8fe6aa779a6a0e2aea091
chitsaou <chitsaou@techbang.com.tw>
2012-02-09 14:56:45 +0800

    re-implement Section in ActiveRecord

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,11 +1,6 @@
 # coding: utf-8  
-class Section
-  include Mongoid::Document
-  include Mongoid::Timestamps
-  include Mongoid::BaseModel
+class Section < ActiveRecord::Base
 
-  field :name
-  field :sort, :type => Integer, :default => 0
   has_many :nodes, :dependent => :destroy
   
   validates_presence_of :name
