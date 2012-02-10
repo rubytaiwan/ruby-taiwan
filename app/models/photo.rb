a67f4082cf553c12bf6d86a618c1fbadf983229f
chitsaou <chitsaou@techbang.com.tw>
2012-02-10 18:29:35 +0800

    re-implement photo in ActiveRecord

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,15 +1,12 @@
 # coding: utf-8  
-class Photo  
-  include Mongoid::Document
-  include Mongoid::Timestamps  
-  include Mongoid::BaseModel
-
-  field :image  
-  
+class Photo < ActiveRecord::Base
   belongs_to :user
   
   attr_protected :user_id
   
+
+  scope :recent, order("id DESC")
+
   # 封面图
   mount_uploader :image, PhotoUploader
   
