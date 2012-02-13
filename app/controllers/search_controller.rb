41d635865bd4ad0e48dfed23cce1372a4d2daea1
chitsaou <chitsaou@techbang.com.tw>
2012-02-13 17:31:59 +0800

    s/@q/@query_string/g

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,40 +3,40 @@ class SearchController < ApplicationController
   
   before_filter :validate_search_key
    def index
-     if @q.present?
-       @topics = Topic.search(@q).paginate(:page => params[:page], :per_page => 20)
+     if @query_string.present?
+       @topics = Topic.search(@query_string).paginate(:page => params[:page], :per_page => 20)
      end
      @current = ["/search/topics?q=#{params[:q]}"]
      
      render :action => "topics"
      
-     set_seo_meta("#{t("common.search")}: #{@q}")
-     drop_breadcrumb("#{t("common.search")}: #{@q}")
+     set_seo_meta("#{t("common.search")}: #{@query_string}")
+     drop_breadcrumb("#{t("common.search")}: #{@query_string}")
   end
 
   def topics
     
-    if @q.present?
-      @topics = Topic.search(@q).paginate(:page => params[:page], :per_page => 20)
+    if @query_string.present?
+      @topics = Topic.search(@query_string).paginate(:page => params[:page], :per_page => 20)
     end
     
-    set_seo_meta("#{t("common.search")}: #{@q}")
-    drop_breadcrumb("#{t("common.search")}: #{@q}")
+    set_seo_meta("#{t("common.search")}: #{@query_string}")
+    drop_breadcrumb("#{t("common.search")}: #{@query_string}")
 
    end
 
   def wiki
-    if @q.present?
-      @results = Wiki.search(@q)
+    if @query_string.present?
+      @results = Wiki.search(@query_string)
     end
     
-    set_seo_meta("#{t("common.search")}: #{@q}")
-    drop_breadcrumb("#{t("common.search")}: #{@q}")
+    set_seo_meta("#{t("common.search")}: #{@query_string}")
+    drop_breadcrumb("#{t("common.search")}: #{@query_string}")
   end
 
   protected
 
     def validate_search_key
-      @q = params[:q].gsub(/\\|\'|-|\/|\.|\?/, "") if params[:q].present?
+      @query_string = params[:q].gsub(/\\|\'|-|\/|\.|\?/, "") if params[:q].present?
     end
 end
